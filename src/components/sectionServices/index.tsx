b0feeca2c6066e7dae195d1fab2e778dc5c8a254
Darlan0307 <darlanchagas2020@gmail.com>
2024-02-29 15:43:59 -0300

    :lipstick: estilização do carrossel finalizada

--- src/components/sectionServices/index.tsx
+++ src/components/sectionServices/index.tsx
@@ -7,21 +7,21 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 const dataServices = [
   {
     id:0,
-    icon: MdHealthAndSafety,
+    icon: <MdHealthAndSafety/>,
     title:"Consultas",
     description:"Exames físicos, avaliação de saúde, diagnóstico de doenças, prescrição de medicamentos e acompanhamento de pacientes.",
     link:"https://www.nucleodoconhecimento.com.br/veterinaria"
   },
   {
     id:1,
-    icon: TbVaccine,
+    icon: <TbVaccine/>,
     title:"Vacinação",
     description:"Imunização contra doenças contagiosas, como raiva, cinomose, parvovirose e leptospirose.",
     link:"https://www.nucleodoconhecimento.com.br/veterinaria"
   },
   {
     id:2,
-    icon: FaTooth,
+    icon: <FaTooth/>,
     title:"Odontologia",
     description:" Limpeza de tártaro, profilaxia bucal, extração de dentes e tratamento de doenças periodontais.",
     link:"https://www.nucleodoconhecimento.com.br/veterinaria"
@@ -34,19 +34,33 @@ const SectionServices = () => {
       <h2 className='subtitle'>Serviços de alta qualidade para você</h2>
 
     <Swiper
-      slidesPerView={2}
       pagination={{ clickable:true }}
       navigation
 			loop= {true}
       className='container-carosel'
+      breakpoints={{
+        0: {
+          slidesPerView: 1,          
+        },
+        800: {
+          slidesPerView: 2,          
+        },
+        1200:{
+          slidesPerView: 3,
+        }
+      }}
     >
 
       {dataServices.map(service =>(
         <SwiperSlide key={service.id} className='carosel-card'>
-          <service.icon/>
+          <div className='container'>
+          <span className='icon-service'>
+            {service.icon}
+          </span>
           <h3>{service.title}</h3>
           <p>{service.description}</p>
           <a href={service.link} target='_blank'>Saber Mais</a>
+          </div>
         </SwiperSlide>
       ))}
    
