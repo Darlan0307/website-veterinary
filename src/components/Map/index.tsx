8d0b9acd2d3d7290976f8d0d89a0f27e8de89265
Darlan0307 <darlanchagas2020@gmail.com>
2024-03-05 10:34:31 -0300

    :bug: possível solução para o bug da api do google maps

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -3,9 +3,9 @@ import './styles.scss'
 const Map = () => {
   return (
     <div className='container-map'>
-      <iframe className='map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63210.881834917745!2d-34.99624187832028!3d-8.031879499999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab19611a499545%3A0x5ded64163bcd95fa!2sAnimalis%20-%20Cirurgia%20e%20Cl%C3%ADnica%20Veterin%C3%A1ria%20-%20Recife!5e0!3m2!1spt-BR!2sbr!4v1709638613295!5m2!1spt-BR!2sbr" loading="lazy"></iframe>
+      <iframe className='map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63210.881834917745!2d-34.99624187832028!3d-8.031879499999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab19611a499545%3A0x5ded64163bcd95fa!2sAnimalis%20-%20Cirurgia%20e%20Cl%C3%ADnica%20Veterin%C3%A1ria%20-%20Recife!5e0!3m2!1spt-BR!2sbr!4v1709645130820!5m2!1spt-BR!2sbr" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
     </div>
-  )
+  )   
 }
 
 export default Map
