2d46b36abaa72d2b2cf9ffdb545b49d90cb78230
Darlan0307 <darlanchagas2020@gmail.com>
2024-03-05 10:08:28 -0300

    :sparkles: estrutura inicial do formulario concluida

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -9,17 +9,17 @@ const Form = () => {
       <form className='form' autoComplete='off'> 
         <div className='wrap-input'>
           <label className='label' htmlFor="name">Nome:</label>
-          <input className='input' type="text" id='name' placeholder='Seu nome'/>
+          <input className='input' type="text" id='name' placeholder='Seu nome' required min={3}/>
         </div>
         <div className='wrap-input'>
           <label className='label' htmlFor="email">Email:</label>
-          <input className='input' type="email" id='email' placeholder='Seu email'/>
+          <input className='input' type="email" id='email' placeholder='Seu email' required/>
         </div>
         <div className='wrap-input'>
           <label className='label' htmlFor="message">Mensagem:</label>
-          <textarea className='text-area'  id="message" placeholder='Sua mensagem'></textarea>
+          <textarea className='text-area'  id="message" placeholder='Sua mensagem' required minLength={10}></textarea>
         </div>
-        <button className='submit' type='button'>Enviar Mensagem</button>
+        <button className='submit' type='submit'>Enviar Mensagem</button>
       </form>
     </div>
   )
